229156088c151468911ffd79fed8d72312b6a9d9
lamtung16 <nguyenlamtung10@gmail.com>
2024-11-12 14:50:27 -0700

    add lopart

--- opart_lda.py
+++ opart_lda.py
@@ -44,3 +44,51 @@ def opart(sequence, lda):
 
     set_of_chpnt = trace_back(tau_star[1:])             # get set of changepoints
     return set_of_chpnt[1:-1] - 1
+
+
+def get_T(t, neg_start, neg_end, pos_start, pos_end): 
+
+    # if t is just outside of pos region
+    for s, e in zip(pos_start, pos_end):
+        if(t == e):
+            T = np.arange(s, e)
+            return T
+    
+    # initiate T = [0, ..., t]
+    T = np.arange(t)
+    
+    # remove negative regions
+    for s, e in zip(neg_start, neg_end):
+        T = T[(T < s) | (T >= e)]
+    
+    # remove positive regions with t > end and t > start
+    for s, e in zip(pos_start, pos_end):
+        if(t < e):
+            T = T[(T < s)]
+        else:
+            T = T[(T >= s)]
+    
+    return T
+
+
+def lopart(sequence, neg_start, neg_end, pos_start, pos_end, lda):
+    sequence = np.append(0, sequence)
+    y, z = get_cumsum(sequence)         # cumsum vector
+    sequence_length = len(sequence)-1   # sequence length
+
+    # Set up
+    C = np.zeros(sequence_length + 1)
+    C[0] = -lda
+
+    # Get tau_star
+    tau_star = np.zeros(sequence_length+1, dtype=int)
+    for t in range(1, sequence_length+1):
+        po_chpnt = get_T(t, neg_start+1, neg_end+1, pos_start+1, pos_end+1)     # get set of possible changepoint
+        V = C[po_chpnt] + lda + L(1 + po_chpnt, t, y, z)                # get set of possible value
+        last_chpnt = po_chpnt[np.argmin(V)]                             # get optimal tau from set V
+        C[t] = V[np.argmin(V)]                                          # update C_i
+        tau_star[t] = last_chpnt                                        # update tau_star
+
+    # get set of changepoints
+    set_of_chpnt = trace_back(tau_star[1:])
+    return set_of_chpnt[1:-1] - 1
